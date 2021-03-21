5b40aa7136fbdc25a896b1a152e42828721ef401
Christine <baba3425@gmail.com>
2021-03-21 18:39:53 +0800

    新增自訂的字串擴充功能

---
+++ WebApplication/WebLibrary/Extensions/StringExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebLibrary
+{
+    public static class StringExtension
+    {
+        /// <summary>
+        /// string轉Int32
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static int ToInt32(this string input)
+        {
+            return int.Parse(input);
+        }
+    }
+}
