36376a39de4033401d48bc8ea95e2f30c57cb0d3
Christine <baba3425@gmail.com>
2021-03-21 13:08:37 +0800

    將自訂的DI服務抽成獨立檔案控管

---
+++ WebApplication/WebApplication/ProjectDI.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication
+{
+    public static class ProjectDI
+    {
+        public static IServiceCollection AddProjectDI(this IServiceCollection services)
+        {
+            // TODO
+            return services;
+        }
+    }
+}
--- WebApplication/WebApplication/Startup.cs
+++ WebApplication/WebApplication/Startup.cs
@@ -23,6 +23,8 @@ namespace WebApplication
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // 專案自訂的依賴注入服務彙整於ProjectDI.cs
+            services.AddProjectDI();
             services.AddControllersWithViews();
         }
 
